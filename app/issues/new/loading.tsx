2f88822126640d60782dab86e68d4a5211089673
CyberStackPro <yeabsragebriel@gmail.com>
2024-11-17 17:07:46 +0300

    Add aditional Loading Skeletons

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Box } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssuePage = () => {
-  return <div>LoadingIssuePage</div>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Skeleton height="2rem" />
+    </Box>
+  );
 };
 
 export default LoadingIssuePage;
