6af70a489daf78807d186d2f4f72eec8a0346a84
CyberStackPro <yeabsragebriel@gmail.com>
2024-11-18 13:59:13 +0300

    Refactor the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -13,14 +13,9 @@ import {
   Flex,
   Text,
 } from "@radix-ui/themes";
+import { stat } from "fs";
 
 const NavBar = () => {
-  const currentPath = usePathname();
-  const { status, data: session } = useSession();
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues/list" },
-  ];
   return (
     <nav className=" border-b py-4 px-5 mb-5  ">
       <Container>
@@ -29,55 +24,70 @@ const NavBar = () => {
             <Link href={"/"}>
               <Bug className="w-6 h-6 text-gray-600 dark:text-gray-300" />
             </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    href={link.href}
-                    className={classNames({
-                      "text-zinc-900 dark:text-zinc-50":
-                        currentPath === link.href,
-                      "text-zinc-600 dark:text-zinc-400":
-                        currentPath !== link.href,
-                      "transition-colors duration-200": true,
-                    })}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    size={"2"}
-                    radius="full"
-                    className="cursor-pointer"
-                    referrerPolicy="no-referrer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size={"2"}>{session.user?.email!}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href={"/api/auth/signout"}>Log Out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href={"/api/auth/signin"}>Log In</Link>
-            )}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return null;
+  if (status === "unauthenticated")
+    return <Link href={"/api/auth/signin"}>Log In</Link>;
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size={"2"}
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size={"2"}>{session!.user?.email!}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href={"/api/auth/signout"}>Log Out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+const NavLinks = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues/list" },
+  ];
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            href={link.href}
+            className={classNames({
+              "text-zinc-900 dark:text-zinc-50": currentPath === link.href,
+              "text-zinc-600 dark:text-zinc-400": currentPath !== link.href,
+              "transition-colors duration-200": true,
+            })}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
