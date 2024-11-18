c37f0295f3aeaa87db04397c80ff602068beac8c
CyberStackPro <yeabsragebriel@gmail.com>
2024-11-18 15:10:29 +0300

    Enhance the Api to assigne issues

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -8,5 +8,24 @@ export const issueSchema = z.object({
   description: z
     .string()
     .min(5, { message: "Description must be at least 5 characters" })
-    .max(255),
+    .max(65535),
+});
+
+export const patchIssueSchema = z.object({
+  title: z
+    .string()
+    .min(5, { message: "Title must be at least 5 characters" })
+    .max(255)
+    .optional(),
+  description: z
+    .string()
+    .min(5, { message: "Description must be at least 5 characters" })
+    .max(65535)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required.")
+    .max(255)
+    .optional()
+    .nullable(),
 });
