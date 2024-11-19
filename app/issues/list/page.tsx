f4e2fd7e93567c8c7d51e4bd1431bfbc865f0215
CyberStackPro <yeabsragebriel@gmail.com>
2024-11-19 21:24:05 +0300

    Sort Issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -4,10 +4,14 @@ import IssueAction from "./IssueAction";
 import { IssueStatusBadge, Link } from "../../components";
 import NextLink from "next/link";
 import { Issue, Status } from "@prisma/client";
-import { ArrowUp } from "lucide-react";
+import { ArrowDown, ArrowUp } from "lucide-react";
 
 interface Props {
-  searchParams: { status: Status; orderBy: keyof Issue };
+  searchParams: {
+    status: Status;
+    orderBy: keyof Issue;
+    sortOrder: "asc" | "desc";
+  };
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
@@ -25,10 +29,17 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : "";
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: searchParams.sortOrder || "asc" }
+    : undefined;
   const issues = await prisma.issue.findMany({
     where: {
       status: status ? status : undefined,
     },
+    orderBy: orderBy,
   });
 
   return (
@@ -41,13 +52,21 @@ const IssuesPage = async ({ searchParams }: Props) => {
               <Table.ColumnHeaderCell key={index} className={column.className}>
                 <NextLink
                   href={{
-                    query: { ...searchParams, orderBy: column.value },
+                    query: {
+                      ...searchParams,
+                      orderBy: column.value,
+                      sortOrder:
+                        searchParams.sortOrder === "asc" ? "desc" : "asc",
+                    },
                   }}
                 >
                   {column.label}
                 </NextLink>
-                {column.value === searchParams.orderBy && (
+                {column.value === searchParams.orderBy &&
+                searchParams.sortOrder === "asc" ? (
                   <ArrowUp className="inline size-3 ml-1" />
+                ) : (
+                  <ArrowDown className="inline size-3 ml-1" />
                 )}
               </Table.ColumnHeaderCell>
             ))}
